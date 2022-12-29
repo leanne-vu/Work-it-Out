5249c82339f72830a24cb80e5cc3af934a1a54ad
Leanne Vu <leannevu1@yahoo.com>
2022-12-29 22:25:14 +0000

    Created react form with necessary required fields.

--- client/pages/form.jsx
+++ client/pages/form.jsx
@@ -1,7 +1,114 @@
 import React from 'react';
-export default function Form(props) {
-  return (
-    <div><h1>Hello World</h1></div>
 
-  );
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleWorkoutChange = this.handleWorkoutChange.bind(this);
+    this.handleMuscleGroupChange = this.handleMuscleGroupChange.bind(this);
+    this.handleRepChange = this.handleRepChange.bind(this);
+    this.handleSetChange = this.handleSetChange.bind(this);
+    this.handleNoteChange = this.handleNoteChange.bind(this);
+    this.state = { date: '', workoutName: '', muscleGroup: '', reps: '', sets: '', notes: '' };
+  }
+
+  handleDateChange(event) {
+    this.setState({ date: event.target.value });
+  }
+
+  handleWorkoutChange(event) {
+    this.setState({ workoutName: event.target.value });
+  }
+
+  handleRepChange(event) {
+    this.setState({ reps: event.target.value });
+  }
+
+  handleSetChange(event) {
+    this.setState({ sets: event.target.value });
+  }
+
+  handleNoteChange(event) {
+    this.setState({ notes: event.target.value });
+  }
+
+  handleMuscleGroupChange(event) {
+    this.setState({ muscleGroup: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ date: '', workoutName: '', muscleGroup: '', reps: '', sets: '', notes: '' });
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <div className="row">
+          <div className="col-2" />
+          <div className="col-8">
+            <h1 className="form-header">Add a Workout
+            </h1>
+          </div>
+        </div>
+        <form className="form-row" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="workout-date">
+              Date
+              <input required className="form-control" name="date" type="date" id="workout-date" onChange={this.handleDateChange} value={this.state.date} />
+            </label>
+          </div>
+          <div className="form-group workout-name-form">
+            <label htmlFor="workout-name">
+              Workout Name
+              <input required placeholder="Workout Name" className="form-control workout-input" name="name" type="text" id="workout-name" onChange={this.handleWorkoutChange} value={this.state.workoutName} />
+            </label>
+          </div>
+          <div className="form-group muscle-group-form">
+            <label className="muscle-group-label" htmlFor="muscle-group">
+              Muscle Group</label>
+            <select name="muscle-group" required onChange={this.handleMuscleGroupChange}>
+              <option value="">Please select</option>
+              <option value="abdominals">abdominals</option>
+              <option value="abductors">abductors</option>
+              <option value="biceps">biceps</option>
+              <option value="calves">calves</option>
+              <option value="chest">chest</option>
+              <option value="forearms">forearms</option>
+              <option value="glutes">glutes</option>
+              <option value="hamstrings">hamstrings</option>
+              <option value="lats">lats</option>
+              <option value="lower back">lower back</option>
+              <option value="middle back">middle back</option>
+              <option value="neck">neck</option>
+              <option value="quadriceps">quadriceps</option>
+              <option value="traps">traps</option>
+              <option value="triceps">triceps</option>
+              <option value="other">other</option>
+            </select>
+          </div>
+          <div className="form-group workout-name-form">
+            <label htmlFor="reps">
+              Reps
+              <input required placeholder="# of Reps" className="form-control" name="reps" type="number" id="reps" onChange={this.handleRepChange} value={this.state.reps} />
+            </label>
+          </div>
+          <div className="form-group set-form workout-name-form">
+            <label className="set-label" htmlFor="sets">
+              Sets
+              <input required placeholder="# of Sets" className="form-control" name="sets" type="number" id="sets" onChange={this.handleSetChange} value={this.state.sets} />
+            </label>
+          </div>
+          <div className="form-group  notes-form">
+            <label className="notes-label" htmlFor="notes">
+              Notes
+              <input placeholder="Additional notes" className="notes-control form-control" name="notes" type="text" id="notes" onChange={this.handleNoteChange} value={this.state.notes} />
+            </label>
+          </div>
+          <button className="form-submit-but" type="submit">Add a workout</button>
+        </form>
+      </div>
+    );
+  }
 }

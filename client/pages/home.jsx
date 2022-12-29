760a1da8d991b0ac6baa3bb3c36ca1b47df3545e
Leanne Vu <leannevu1@yahoo.com>
2022-12-29 18:07:55 +0000

    Utilize hashrouting to take users to form #1.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Header from '../components/hello-world';
 
 export default function Home(props) {
   return (
     <div>
-      <div>
-        <Header />
-      </div>
       <div className="background-container">
         <div className="bg-img background" />
-        <button className="workout-but">Add a Workout</button>
+        <a href="#form" className="workout-but">Add a Workout</a>
       </div>
     </div>
   );

ae8748767c2a1576ec257501cd9c8f116239c5ea
Leanne Vu <leannevu1@yahoo.com>
2022-12-30 07:48:31 +0000

    Completed form from react, setstate, fixed stylization issues. #1

--- client/pages/form.jsx
+++ client/pages/form.jsx
@@ -40,6 +40,7 @@ export default class Form extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     this.setState({ date: '', workoutName: '', muscleGroup: '', reps: '', sets: '', notes: '' });
+
   }
 
   render() {
@@ -52,59 +53,68 @@ export default class Form extends React.Component {
             </h1>
           </div>
         </div>
-        <form className="form-row" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="workout-date">
-              Date
-              <input required className="form-control" name="date" type="date" id="workout-date" onChange={this.handleDateChange} value={this.state.date} />
-            </label>
+        <form onSubmit={this.handleSubmit}>
+          <div className="row">
+            <div className="column-full-date">
+              <label htmlFor="workout-date">
+                Date
+                <input required className="form-control" name="date" type="date" id="workout-date" onChange={this.handleDateChange} value={this.state.date} />
+              </label>
+            </div>
           </div>
-          <div className="form-group workout-name-form">
-            <label htmlFor="workout-name">
-              Workout Name
-              <input required placeholder="Workout Name" className="form-control workout-input" name="name" type="text" id="workout-name" onChange={this.handleWorkoutChange} value={this.state.workoutName} />
-            </label>
+          <div className="row">
+            <div className="workoutname-column column-half">
+              <label htmlFor="workout-name">
+                Workout Name
+                <input required placeholder="Workout Name" className="form-control workout-input" name="name" type="text" id="workout-name" onChange={this.handleWorkoutChange} value={this.state.workoutName} />
+              </label>
+            </div>
+            <div className="musclegroup-column column-half">
+              <label className="muscle-group-label" htmlFor="muscle-group">
+                Muscle Group
+                <select className="muscle-group-select" name="muscle-group" required onChange={this.handleMuscleGroupChange}>
+                  <option value="">Please select</option>
+                  <option value="abdominals">abdominals</option>
+                  <option value="abductors">abductors</option>
+                  <option value="biceps">biceps</option>
+                  <option value="calves">calves</option>
+                  <option value="chest">chest</option>
+                  <option value="forearms">forearms</option>
+                  <option value="glutes">glutes</option>
+                  <option value="hamstrings">hamstrings</option>
+                  <option value="lats">lats</option>
+                  <option value="lower back">lower back</option>
+                  <option value="middle back">middle back</option>
+                  <option value="neck">neck</option>
+                  <option value="quadriceps">quadriceps</option>
+                  <option value="traps">traps</option>
+                  <option value="triceps">triceps</option>
+                  <option value="other">other</option>
+                </select>
+              </label>
+            </div>
           </div>
-          <div className="form-group muscle-group-form">
-            <label className="muscle-group-label" htmlFor="muscle-group">
-              Muscle Group</label>
-            <select name="muscle-group" required onChange={this.handleMuscleGroupChange}>
-              <option value="">Please select</option>
-              <option value="abdominals">abdominals</option>
-              <option value="abductors">abductors</option>
-              <option value="biceps">biceps</option>
-              <option value="calves">calves</option>
-              <option value="chest">chest</option>
-              <option value="forearms">forearms</option>
-              <option value="glutes">glutes</option>
-              <option value="hamstrings">hamstrings</option>
-              <option value="lats">lats</option>
-              <option value="lower back">lower back</option>
-              <option value="middle back">middle back</option>
-              <option value="neck">neck</option>
-              <option value="quadriceps">quadriceps</option>
-              <option value="traps">traps</option>
-              <option value="triceps">triceps</option>
-              <option value="other">other</option>
-            </select>
+          <div className="row">
+            <div className="rep-column column-half">
+              <label htmlFor="reps">
+                Reps
+                <input required placeholder="# of Reps" className="form-control" name="reps" type="number" id="reps" onChange={this.handleRepChange} value={this.state.reps} />
+              </label>
+            </div>
+            <div className="set-column column-half">
+              <label className="set-label" htmlFor="sets">
+                Sets
+                <input required placeholder="# of Sets" className="form-control" name="sets" type="number" id="sets" onChange={this.handleSetChange} value={this.state.sets} />
+              </label>
+            </div>
           </div>
-          <div className="form-group workout-name-form">
-            <label htmlFor="reps">
-              Reps
-              <input required placeholder="# of Reps" className="form-control" name="reps" type="number" id="reps" onChange={this.handleRepChange} value={this.state.reps} />
-            </label>
-          </div>
-          <div className="form-group set-form workout-name-form">
-            <label className="set-label" htmlFor="sets">
-              Sets
-              <input required placeholder="# of Sets" className="form-control" name="sets" type="number" id="sets" onChange={this.handleSetChange} value={this.state.sets} />
-            </label>
-          </div>
-          <div className="form-group  notes-form">
-            <label className="notes-label" htmlFor="notes">
-              Notes
-              <input placeholder="Additional notes" className="notes-control form-control" name="notes" type="text" id="notes" onChange={this.handleNoteChange} value={this.state.notes} />
-            </label>
+          <div className="row">
+            <div className="column-full-date">
+              <label className="notes-label" htmlFor="notes">
+                Notes
+                <input placeholder="Additional notes" className="notes-control form-control" name="notes" type="text" id="notes" onChange={this.handleNoteChange} value={this.state.notes} />
+              </label>
+            </div>
           </div>
           <button className="form-submit-but" type="submit">Add a workout</button>
         </form>

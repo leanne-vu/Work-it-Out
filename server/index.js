466212caf38d068f24876e220852fc2e64547b2c
Leanne Vu <leannevu1@yahoo.com>
2023-01-03 18:37:27 +0000

    Fixed status error and server crashing upon duplications. #1

--- server/index.js
+++ server/index.js
@@ -17,42 +17,62 @@ app.use(staticMiddleware);
 app.post('/api/exercises', (req, res, next) => {
 
   const { date, workoutName, muscleGroup, reps, sets, notes } = req.body;
-
   const sql = `
+select *
+from "Workouts"
+where "Date" = $1
+`;
+  const params = [date + 'T00:00:00Z'];
+  db.query(sql, params)
+    .then(result => {
+      if (result.rows.length < 1) {
+        res.status(201);
+      } else {
+        throw new ClientError(400, 'date already exists');
+      }
+    })
+    .then(data => {
+      const sql = `
   insert into "Workouts" ("Date")
   values ($1)
   returning *
   `;
-  const params = [date];
-  db.query(sql, params)
-    .then(result => {
-      const [workout] = result.rows;
-      res.status(201).json(workout);
-      return (workout.WorkoutID);
-    })
-    .then(data => {
-      if (!workoutName || !muscleGroup || !reps || !sets) {
-        throw new ClientError(400, 'workoutName, muscleGroup, reps, sets are required fields');
-      } else {
-        const sql = `
+      const params = [date];
+
+      db.query(sql, params)
+        .then(result => {
+          const [workout] = result.rows;
+          return (workout.WorkoutID);
+        })
+        .then(data => {
+          if (muscleGroup === '') {
+            throw new ClientError(404, 'muscleGroup cannot be empty');
+          }
+          if (!workoutName || !muscleGroup || !reps || !sets) {
+            throw new ClientError(400, 'workoutName, muscleGroup, reps, sets are required fields');
+          } else {
+            const sql = `
         insert into "Exercises"("WorkoutID","WorkoutName", "MuscleGroup", "Sets", "Reps", "Notes" )
         values ( $1, $2, $3, $4, $5, $6)
         returning *
         `;
-        const params = [data, workoutName, muscleGroup, sets, reps, notes];
-        db.query(sql, params)
-          .then(result => {
-            const [exercise] = result.rows;
-            res.status(201).json(exercise);
-          })
-          .catch(err => { next(err); });
-      }
-    }
-
-    )
-    .catch(err => {
-      next(err);
-    });
+            const params = [data, workoutName, muscleGroup, sets, reps, notes];
+            db.query(sql, params)
+              .then(result => {
+                const [exercise] = result.rows;
+                res.status(201).json(exercise);
+              })
+              .catch(err => {
+                next(err);
+              });
+            // }
+          }
+        }
+        )
+        .catch(err => {
+          next(err);
+        });
+    }).catch(err => { next(err); });
 });
 
 app.use(errorMiddleware);

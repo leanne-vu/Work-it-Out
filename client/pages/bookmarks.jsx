b6a4dad008e3483a23c4dce9d0a17575f16f16a9
Leanne Vu <leannevu1@yahoo.com>
2023-01-26 08:56:45 +0000

    Added heading for when there are no bookmarks. #7

--- client/pages/bookmarks.jsx
+++ client/pages/bookmarks.jsx
@@ -31,27 +31,35 @@ export default class Bookmarks extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="idea-list">
-          <ul className="idea-ul">
-            {this.state.bookmarks.map(x => {
-              return (
-                <li className="workout-ideas" key={x.ExerciseName}>
-                  <div>
-                    <h1 className="name-ideas">{x.ExerciseName} <i onClick={e => this.deleteIdea(this.state.bookmarks.find(item => item.name === x.name))} className="used-star fa-sharp fa-solid fa-star" />
-                    </h1>
-                    <h3 className="muscle-name-ideas">Muscle: {x.MuscleGroup}</h3>
-                    <h3 className="equipment-ideas">Equipment: {x.Equipment}</h3>
-                    <h3 className="instructions-ideas">Instructions: {x.Info}</h3>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+    if (this.state.bookmarks.length > 0) {
+      return (
+        <div>
+          <div className="idea-list">
+            <ul className="idea-ul">
+              {this.state.bookmarks.map(x => {
+                return (
+                  <li className="workout-ideas" key={x.ExerciseName}>
+                    <div>
+                      <h1 className="name-ideas">{x.ExerciseName} <i onClick={e => this.deleteIdea(this.state.bookmarks.find(item => item.name === x.name))} className="used-star fa-sharp fa-solid fa-star" />
+                      </h1>
+                      <h3 className="muscle-name-ideas">Muscle: {x.MuscleGroup}</h3>
+                      <h3 className="equipment-ideas">Equipment: {x.Equipment}</h3>
+                      <h3 className="instructions-ideas">Instructions: {x.Info}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <h2 className="no-workout">There are no bookmarks to display.</h2>
+        </div>
+      );
+    }
   }
 
 }
